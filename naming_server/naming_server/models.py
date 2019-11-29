959de6abd3cd561ba611baaaae491c036639380b
Madina Gafarova <madina.gafarova@yandex.ru>
2019-11-29 23:53:45 +0300

    Add models to system
    Add Files model
    Add Directory model
    Add Storage model
    Add StoredFile model (as relation's attribute)

--- naming_server/naming_server/models.py
+++ naming_server/naming_server/models.py
@@ -1,1 +1,54 @@
 from django.db import models
+
+
+class Directory(models.Model):
+    name = models.CharField(max_length=255)
+    parent_dir = models.ForeignKey(
+        'Directory',
+        on_delete=models.CASCADE,
+        related_name='subdirs',
+    )
+
+
+class Storage(models.Model):
+    id = models.UUIDField()
+    files = models.ManyToManyField(
+        'Storage',
+        related_name='files',
+        through='StoredFile',
+    )
+    available_size = models.IntegerField()
+    last_heartbeat = models.DateTimeField()
+
+
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    size = models.IntegerField()
+    parent_dir = models.ForeignKey(
+        'Directory',
+        on_delete=models.CASCADE,
+        related_name='files',
+    )
+
+
+class StoredFile(models.Model):
+    UPLOADING = 'UPL'
+    READY = 'RDY'
+    DELETING = 'DEL'
+    MOVING = 'MOV'
+
+    STATUSES = [
+        (UPLOADING, 'Uploading'),
+        (READY, 'Ready'),
+        (DELETING, 'Deleting'),
+        (MOVING, 'Moving'),
+    ]
+
+    status = models.CharField(
+        max_length=3,
+        choices=STATUSES,
+        null=True,
+    )
+
+    file = models.ForeignKey('File', on_delete=models.CASCADE)
+    storage = models.ForeignKey('Storage', on_delete=models.CASCADE)
