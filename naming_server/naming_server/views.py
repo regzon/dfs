e8ffedf6b86cf8e295e1cce91803bdf101b9c397
Madina Gafarova <madina.gafarova@yandex.ru>
2019-11-30 20:44:50 +0300

    Add view of downloading file

--- naming_server/naming_server/views.py
+++ naming_server/naming_server/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+import json
+
 DEF_SIZE = 1024
 
 
@@ -11,3 +13,41 @@ def init(request):
         response['status'] = 'error'
         response['error'] = f'Not correct method type. Get {request.method}\
             insted POST'
+
+
+def create_file(request):
+    response = HttpResponse()
+    if request.method == 'POST':
+        # TODO: get from StorageNode
+        upload_url = ''
+        response['status'] = 'success'
+        response['data']['upload_url'] = upload_url
+    else:
+        response['status'] = 'error'
+        response['error'] = f'Not correct method type. Get {request.method}\
+            insted POST'
+
+
+def read_file(request):
+    response = HttpResponse()
+    if request.method == 'GET':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        path = body['path']
+        if is_exists(path):
+            # TODO: get from StorageNode
+            url_to_file = ''
+            response['status'] = 'success'
+            response['data']['download_url'] = url_to_file
+        else:
+            response['status'] = 'error'
+            response['error'] = 'Path/File does not exists'
+
+    else:
+        response['status'] = 'error'
+        response['error'] = f'Not correct method type. Get {request.method}\
+            insted GET'
+
+
+def is_exists(file_path):
+    pass
